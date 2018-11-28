bd0dfa5e4317a256f7f0978866c9a58683795251
Tyler Weaver <tylerjw@gmail.com>
2018-11-27 18:59:00 -0700

    can_test.py

---
+++ can_test.py
@@ -0,0 +1,40 @@
+import os, can
+
+def stop_can():
+  os.system('sudo ifconfig can0 down')
+
+PID_RPM             = 0x0C
+PID_VEHICLE_SPEED   = 0x0D
+PID_COOLANT_TEMP    = 0x05
+PID_FUEL_LEVEL      = 0x2F
+
+if __name__ == '__main__':
+  # setup can
+  os.system('sudo ip link set can0 type can bitrate 100000')
+  os.system('sudo ifconfig can0 up')
+  can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')# socketcan_native
+  a_listener = can.BufferedReader()
+  notifier = can.Notifier(can0, [a_listener])
+
+  rpm = 0
+  speed_kph = 0
+  water_c = 0
+  fuel_level = 0
+
+  while True:
+    # update the values
+    m = a_listener.get_message(0)
+    while m:
+      if (m.arbitration_id == PID_RPM):
+        rpm = (m.data[0]<<8 + m.data[1]) / 4
+      elif (m.arbitration_id == PID_VEHICLE_SPEED):
+        speed_kph = m.data[0]
+      elif (m.arbitration_id == PID_COOLANT_TEMP):
+        water_c = m.data[0] - 40
+      elif (m.arbitration_id == PID_FUEL_LEVEL):
+        fuel_level = m.data[0] * 100. / 255
+      else:
+        print(m)
+      m = a_listener.get_message(0)
+
+    print("rpm: {}, speed: {}, water: {}, fuel: {}".format(rpm,speed_kph,water_c,fuel_level))
